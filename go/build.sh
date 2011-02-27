264a15d3f02de16a5616843c75b5be7488758ae0
paul wisehart <paul@oldcode.org>
2011-02-27 16:43:32 -0500

    refine build script some

--- go/build.sh
+++ go/build.sh
@@ -2,18 +2,20 @@
 
 CMP="6g"
 LNK="6l"
-
+BINNAME="hello.go.bin"
 
 build() {
     $CMP my_package.go
     $CMP hello.go
 
-    $LNK hello.6 #no need link my_package
+    #no need link my_package
+    $LNK -o $BINNAME hello.6
 }
 
 clean() {
-    rm *.6
-    rm 6.out
+    rm -f *.6
+    rm -f 6.out
+    rm -f $BINNAME
 }
 
 case "$1" in
@@ -24,5 +26,6 @@ case "$1" in
         clean
         ;;
     *)
-        echo "usage $0 build|clean"
+        echo "usage $0 build|clean <--defaulting to 'build'"
+        build
 esac
