ad4d125c92b5057b8b3785893b7f0d1ce036181a
paul wisehart <paul@oldcode.org>
2011-03-01 23:00:30 -0500

    add basic arg processing

--- go/antfilter.go
+++ go/antfilter.go
@@ -1,23 +1,42 @@
 package main
 
 import (
-	"flag"  // command line option parser
+	"os"
+	"flag"
 	"fmt"
-	"./file"
+	//"./file"
 )
 
+var (
+	fHelp *bool = flag.Bool("h", false, "this message")
+	fDebug *bool = flag.Bool("D", false, "echo debug info")
+	fFilterFile *string = flag.String("f", "", "filter file")
+)
+var Usage = func() {
+    fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+	//antfilter [-h][-f <filter-file>][-D] <files-to-filte
+    flag.PrintDefaults()
+}
+
 func main() {
+	flag.Parse()
 
-	flag.Parse()   // Scans the arg list and sets up flags
+	if *fHelp {
+		//flag.PrintDefaults()
+		Usage()
+		return
+	}
 
-	fmt.Printf("start...\n")
+	if *fFilterFile == "" {
+		fmt.Fprintln(os.Stderr, "fatal; please specify -f for filter-file")
+		return
+	}
 
 	for i := 0; i < flag.NArg(); i++ {
-        //os.Stdout.WriteString(s)
 		fmt.Printf(flag.Arg(i) + "\n")
 	}
 
-	file.Read()
+	//file.Read()
 }
 
 
